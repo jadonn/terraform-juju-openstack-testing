f7cd64c91fde30f4625361d1178f8cba1379df97
Jadon Naas <jadon.naas@canonical.com>
2023-02-06 19:49:56 -0500

    Works to deploy machines with MAAS I ran this version of the playbook to deploy 4 machines using virtual baremetal servers and MAAS

--- main.tf
+++ main.tf
@@ -12,9 +12,6 @@ terraform {
 }
 
 provider "juju" {
-    controller_addresses = "10.0.0.60:17070"
-    username = "tf-ovb"
-    password = "tf-ovb"
 }
 
 resource "juju_model" "ovb" {
@@ -30,22 +27,26 @@ resource "juju_machine" "ovb_one" {
     model = juju_model.ovb.name
     series = "jammy"
     name = "ovb-one"
+    constraints = "tags=compute"
 }
 
 resource "juju_machine" "ovb_two" {
     model = juju_model.ovb.name
     series = "jammy"
     name = "ovb-two"
+    constraints = "tags=compute"
 }
 
 resource "juju_machine" "ovb_three" {
     model = juju_model.ovb.name
     series = "jammy"
     name = "ovb-three"
+    constraints = "tags=compute"
 }
 
 resource "juju_machine" "ovb_four" {
     model = juju_model.ovb.name
     series = "jammy"
     name = "ovb-four"
+    constraints = "tags=compute"
 }
