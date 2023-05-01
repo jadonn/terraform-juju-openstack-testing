5cc1f4ea6a1a8e78b6f371c10118324b9a1d3b34
Jadon Naas <jadon.naas@canonical.com>
2023-05-01 14:08:33 +0200

    Fix typo in variable declaration

--- ceph/main.tf
+++ ceph/main.tf
@@ -51,11 +51,11 @@ variable "relation_names" {
 }
 
 output "application_names" {
-    value = object({
+    value = {
         osds = juju_application.ceph_osds.name
         mons = juju_application.ceph_mon.name
         rgw = juju_application.ceph_radosgw.name
-    })
+    }
 }
 
 resource "juju_application" "ceph_osds" {
