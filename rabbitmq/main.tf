2bb0c79dcb2c7c00af1a0ef25aa3c4e092f68ca6
Jadon Naas <jadon.naas@canonical.com>
2023-10-16 14:38:40 -0400

    Missing required_providers

--- rabbitmq/main.tf
+++ rabbitmq/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+      juju = {
+        version = "~> 0.8.0"
+        source  = "juju/juju"
+      }
+    }
+}
+
 resource "juju_application" "rabbitmq" {
     model = var.juju_model
     name = "rabbitmq-server"
