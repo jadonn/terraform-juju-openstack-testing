f0043c4f26fdd1df79df74a39a209c29f89499be
Jadon Naas <jadon.naas@canonical.com>
2023-10-17 15:55:36 -0400

    Remove required_providers

--- placement/main.tf
+++ placement/main.tf
@@ -1,12 +1,3 @@
-terraform {
-    required_providers {
-        juju = {
-            version = "~> 0.8.0"
-            source = "juju/juju"
-        }
-    }
-}
-
 resource "juju_application" "placement" {
     model = var.model
     name = "placement"
